a6e5e191043202cd6fe2eb0634dbf82e8cba631a
851930791 <851930791@qq.com>
2017-08-10 17:27:56 +0800

    first commit

---
+++ src/main/java/com/ruanyinkeji/TestSpirngCloudApplication.java
@@ -0,0 +1,12 @@
+package com.ruanyinkeji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpirngCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSpirngCloudApplication.class, args);
+	}
+}
